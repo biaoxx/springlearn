ee0208222398b1fc7af19fd45bf3f1e690afef99
biao <2915184934@qq.com>
2019-09-15 19:14:03 +0800

    string

---
+++ src/main/java/com/biao/test/Beans/Hello.java
@@ -0,0 +1,12 @@
+package com.biao.test.Beans;
+
+public class Hello {
+
+    private String message;
+    public void setMessage(String message){
+        this.message  = message;
+    }
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+}
